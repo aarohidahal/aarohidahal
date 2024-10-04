1714a5cd75b13ad7b258ee7ffecc630e8c35d866
aarohidahal <arohi.dahal@koshistjames.edu.np>
2024-10-04 10:20:48 +0545

    we did java script

---
+++ E-com-frontend-main/E-com-frontend-main/script.js
@@ -0,0 +1,26 @@
+// script.js carousel image
+const images = [
+    "./img/heroproduct.png",
+    "./img/headphone13.jpg",
+    "./img/headphone15.jpg"
+];
+let cureentIndex = 0;
+const carouselImage = document.getElementById("carouselImage");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+function updateImage() {
+    carouselImage.src = images[currentIndex];
+}
+// Function to move to the next image
+function nextImage() {
+    currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 :0;
+    updateImage();
+}
+// Event listeners for butttons
+prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
+    updateImage();
+})
+nextBtn.addEventListener("click", nextImage);
+// Auto move the carousel every4 seconds
+setInterval(nextImage, 4000); 
